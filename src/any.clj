8389fe2f4b6ea85c0d65e9099ac5b69e8b6dfd1d
molarmanful <benjinpang@gmail.com>
2023-11-13 11:21:21 -0500

    nice

---
+++ src/any.clj
@@ -0,0 +1,20 @@
+(ns any)
+
+(defrecord NUM [x])
+(defrecord STR [x])
+(defrecord CMD [x])
+
+(defmulti toNUM :type)
+(defmulti wNUM :type)
+
+(defmulti toSTR :type)
+(defmulti wSTR :type)
+
+(defmulti toCMD :type)
+(defmulti wCMD :type)
+
+(defmethod wNUM :default [x] (NUM x))
+
+(defmethod wSTR :default [x] (STR x))
+
+(defmethod wCMD :default [x] (CMD x))
--- src/parser.clj
+++ src/parser.clj
@@ -1,6 +1,7 @@
 (ns parser
   (:require [clojure.string :as str])
-  (:require [clojure.core.match :refer [match]]))
+  (:require [clojure.core.match :refer [match]])
+  (:require [any]))
 
 (def ^:private Pt #{:UN :STR :NUM :CMD :ESC :DEC})
 
@@ -17,18 +18,33 @@
 
 (defn pesc [p c])
 
-(defn clean [{:keys [xs x t]}] (concat xs (match t :ESC ())))
+(defn clean
+  [{:keys [xs x t]}]
+  (assoc dParser
+    :xs (concat xs
+                (match t
+                  :ESC [(any/wSTR (conj x \\))]
+                  :STR [(any/wSTR x)]
+                  :CMD [(if (every? (fn [c] (contains? "([{}])" c)) x)
+                          (map (comp any/wSTR str) x)
+                          (any/wCMD x))]
+                  :DEC (match x
+                         "." [(any/wCMD ".")]
+                         (\. :<< last) [(any/wCMD butlast x)]
+                         :else [(any/wNUM x)])
+                  :NUM [(any/wNUM x)]
+                  :else []))))
 
 (defn choice
-  [{:keys [xs x t], :as p} c]
+  [{:keys [_ _ t], :as p} c]
   (match t
     :ESC (pesc p c)
     :STR (pstr p c)
     :else (match c
             \" (assoc (clean p) :t :STR)
             \. (pdot p)
-            (_ :guard #(Character/isDigit %)) (pnum p c)
-            (_ :guard #(Character/isWhitespace %)) (clean p)
+            (true :<< Character/isDigit) (pnum p c)
+            (true :<< Character/isWhitespace) (clean p)
             :else (pcmd p c))))
 
 (defn parse-line [s] (reduce choice dParser s))
