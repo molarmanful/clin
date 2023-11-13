36153b528bf28f8d2e8b6d35b9b13f6ad1cf555f
molarmanful <benjinpang@gmail.com>
2023-11-13 17:47:43 -0500

    WIP

--- src/clin/main.clj
+++ src/clin/main.clj
@@ -1,4 +1,5 @@
 (ns clin.main
-  (:require [clin.parser :as parser]))
+  (:require [clin.parser :as parser])
+  (:gen-class))
 
 (defn -main [& args] (print (parser/parse "1234\"mafo\"asdf2+.3.xyz")))
